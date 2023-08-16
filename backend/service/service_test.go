84a69816c3ccffc1ae568f068b3ad5e5b8c245b9
tranthelam <lamtt2403@gmail.com>
2023-08-16 21:40:37 +0700

    add unit test

--- backend/service/service_test.go
+++ backend/service/service_test.go
@@ -47,35 +47,61 @@ func TestPutStateWrongChannel(t *testing.T) {
 			args:        []string{"key1", "value1"},
 			codeError:   EndorsedError,
 		},
+		{
+			name:        "Wrong chaincode",
+			chainCodeID: "basic1",
+			channelID:   "mychannel",
+			function:    "CreateKey",
+			args:        []string{"key1", "value1"},
+			codeError:   EndorsedError,
+		},
+		{
+			name:        "Wrong function",
+			chainCodeID: "basic",
+			channelID:   "mychannel",
+			function:    "CreateKey1",
+			args:        []string{"key1", "value1"},
+			codeError:   EndorsedError,
+		},
+		{
+			name:        "Success",
+			chainCodeID: "basic",
+			channelID:   "mychannel",
+			function:    "CreateKey",
+			args:        []string{"key1", "value1"},
+			codeError:   0,
+		},
 	}
 
 	for _, test := range testcase {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
 			err := svc.PutState(ctx, test.chainCodeID, test.channelID, test.function, test.args)
-			switch e := err.(type) {
-			case Error:
-				if e.Code != test.codeError {
-					t.Errorf("Error c: %s", e)
+			if err != nil {
+				switch e := err.(type) {
+				case Error:
+					if e.Code != test.codeError {
+						t.Errorf("Error c: %s", e)
+					}
+				default:
+					t.Errorf("Error: %s", e)
 				}
-			default:
-				t.Errorf("Error: %s", e)
 			}
 		})
 	}
 }
 
-func TestPutStateSuccess(t *testing.T) {
-	// chainCodeID := "basic"
-	// channelID := "mychannel"
-	// function := "CreateKey"
-	// args := []string{"key1", "value1"}
-	// err := svc.PutState(ctx, chainCodeID, channelID, function, args)
-	// if err != nil {
-	// 	t.Errorf("Error: %s", err)
-	// }
-}
-
 func TestGetState(t *testing.T) {
+	chainCodeID := "basic"
+	channelID := "mychannel"
+	function := "QueryKey"
+	args := []string{"key1"}
+	res, err := svc.GetState(ctx, chainCodeID, channelID, function, args)
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
 
+	if res != "value1" {
+		t.Errorf("Error: %s", err)
+	}
 }
