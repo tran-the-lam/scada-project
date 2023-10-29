f36934f3e74e82aab86e62047cac02565326328b
tran-the-lam <lamtt2403@gmail.com>
2023-10-29 22:41:45 +0700

    update ui login

--- application/mydemo/lib/screen/login.dart
+++ application/mydemo/lib/screen/login.dart
@@ -26,7 +26,8 @@ class _LoginPageState extends State<LoginPage> {
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
     return deviceInfo.toString();
   }
-TextEditingController _userIDController = TextEditingController();
+
+  TextEditingController _userIDController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
   bool _isLoading = false;
@@ -75,34 +76,95 @@ TextEditingController _userIDController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: _userIDController,
-                    decoration: InputDecoration(labelText: 'UserID'),
+    return MaterialApp(
+      // title: 'Profile',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: Text('Login'),
+        // ),
+        backgroundColor: Color.fromARGB(255, 251, 252, 238),
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Center(
+                
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Container(
+                    width: 500,
+                    height: 400,
+                    child: Column(
+                      children: [
+                        Text(
+                          'SCADA',
+                          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, fontFamily: 'Lobster'),
+                        ),
+                        SizedBox(height: 80.0),
+                        TextField(
+                          controller: _userIDController,
+                          decoration: InputDecoration(
+                            labelText: 'Mã nhân viên',
+                            labelStyle:
+                                TextStyle(color: Colors.blue, fontSize: 18),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            prefixIcon:
+                                Icon(Icons.perm_identity, color: Colors.blue),
+                          ),
+                        ),
+                        SizedBox(height: 30.0),
+                        TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Mật khẩu',
+                            labelStyle:
+                                TextStyle(color: Colors.blue, fontSize: 18),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.blue),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.green),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            prefixIcon: Icon(Icons.lock, color: Colors.blue),
+                          ),
+                        ),
+                        SizedBox(height: 40.0),
+                        ElevatedButton(
+                          child: Text('Đăng nhập'),
+                          onPressed: _login,
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.blue),
+                            padding: MaterialStateProperty.all<EdgeInsets>(
+                              EdgeInsets.symmetric(
+                                  horizontal: 40, vertical: 15),
+                            ),
+                            textStyle: MaterialStateProperty.all<TextStyle>(
+                              TextStyle(fontSize: 18),
+                            ),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(labelText: 'Password'),
-                  ),
-                  SizedBox(height: 16.0),
-                  ElevatedButton(
-                    child: Text('Login'),
-                    onPressed: _login,
-                  ),
-                ],
+                ),
               ),
-            ),
+      ),
     );
   }
 }
-
